2f7245824e89ed706ab787551e8bcf641a87d495
Rui Teng <rui.teng@foxmail.com>
2021-08-22 10:52:58 +0800

    Answer 1: v2
    Signed-off-by: Rui Teng <rui.teng@foxmail.com>

--- LeetCode/1.Two_Sum/Solution.py
+++ LeetCode/1.Two_Sum/Solution.py
@@ -1,16 +1,10 @@
-import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sorted_index = sorted(range(len(nums)), key = lambda i: nums[i])
-        i = 0
-        j = len(nums) - 1
-        while i < j:
-            sum_value = nums[sorted_index[i]] + nums[sorted_index[j]]
-            if sum_value > target:
-                j -= 1
-            elif sum_value < target:
-                i += 1
+        search_map = {}
+        for i, num in enumerate(nums):
+            expected_value = target - num
+            if expected_value in search_map:
+                return [i, search_map[expected_value]]
             else:
-                return [sorted_index[i], sorted_index[j]]
+                search_map[num] = i
         return []
